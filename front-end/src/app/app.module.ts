6fbac44d5183b1e853edd10a80102d5d17a8e83b
hubbellw <hubbellw@xavier.edu>
2020-04-15 17:37:18 -0500

    Created Main Screen Component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './login/login.component';
   ],
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainScreenComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
